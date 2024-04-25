83bc0e3fdd7662e396cb9e3c649229b8d6edf65a
Bens Bernard <48632515+tibernard@users.noreply.github.com>
2024-04-25 14:34:08 -0500

    Delete ABC framework directory

--- ABC framework/archive/Student.py
+++
@@ -1,8 +0,0 @@
-from Person import Person
-class Student(Person):
-  def __init__(self, fname, lname, year):
-    super().__init__(fname, lname)
-    self.graduationyear = year
-
-x = Student("Mike", "Olsen", 2019)
-print(x.graduationyear)
